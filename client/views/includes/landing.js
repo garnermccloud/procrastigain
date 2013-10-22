a34d38a6293d6ff6daf413da2236d15080668e75
Ubuntu <ubuntu@ip-172-31-43-4.us-west-2.compute.internal>
2013-10-22 16:50:51 +0000

    finished landing page

--- client/views/includes/landing.js
+++ client/views/includes/landing.js
@@ -4,3 +4,129 @@ Template.landing.helpers({
 	return "";
     }
 });
+
+
+Template.landing.events({
+    'click .gLogin': function(e) {
+	e.preventDefault();
+	Meteor.loginWithGoogle({requestPermissions: ['email']}, function(error) {
+	    if (error) {
+		throwError('Error logging in with Google');
+	    }
+	});
+    },
+
+    'click .fbLogin': function(e) {
+        e.preventDefault();
+        Meteor.loginWithFacebook({requestPermissions: ['email']}, function(error) {
+            if (error) {
+                throwError('Error logging in with Facebook');
+            }
+        });
+    },
+
+    'submit #topForm': function(e, t) {
+	e.preventDefault();
+	var username = t.find('#username1').value;
+	var email = t.find('#email1').value;
+	var password = t.find('#password1').value;
+	
+	//username validation
+	var validateUsername = function(username) {
+	    
+	    if (username.length >= 3 && username.length <= 20) {
+		return true;
+	    }
+	    else if (username.length > 20) {
+		throwError("Username cannot be greater than 20 characters long");
+		return false;
+	    }
+	    else {
+		throwError("Username must be at least 3 characters long");
+		return false;
+	    }
+	};
+	
+	var validateEmail = function(email) {
+	    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	    
+	    if (re.test(email)) {
+		return true;
+	    } else {
+		throwError("Invalid email");
+		return false;
+	    }
+	};
+	
+	var validatePassword = function(password) {
+	    if (password.length >= 6) {
+		return true;
+	    } else {
+		throwError("Password must be at least 6 characters long");
+		return false;
+	    }
+	};
+	
+	if (validateUsername(username) && validateEmail(email) && validatePassword(password)) {
+	    Accounts.createUser({username: username, email: email, password : password}, function(error){
+		if (error) {
+		    throwError(error.reason);
+		}
+	    });
+	}
+        //return false; 
+    },
+
+    'submit #bottomForm': function(e, t) {
+        e.preventDefault();
+        var username = t.find('#username2').value;
+        var email = t.find('#email2').value;
+        var password = t.find('#password2').value;
+	
+        //username validation
+        var validateUsername = function(username) {
+	    
+            if (username.length >= 3 && username.length <= 20) {
+                return true;
+            }
+            else if (username.length > 20) {
+                throwError("Username cannot be greater than 20 characters long");
+                return false;
+            }
+            else {
+                throwError("Username must be at least 3 characters long");
+                return false;
+            }
+        };
+	
+        var validateEmail = function(email) {
+            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	    
+            if (re.test(email)) {
+                return true;
+            } else {
+                throwError("Invalid email");
+                return false;
+            }
+        };
+	
+        var validatePassword = function(password) {
+            if (password.length >= 6) {
+                return true;
+            } else {
+                throwError("Password must be at least 6 characters long");
+                return false;
+            }
+        };
+	
+        if (validateUsername(username) && validateEmail(email) && validatePassword(password)) {
+            Accounts.createUser({username: username, email: email, password : password}, function(error){
+                if (error) {
+                    throwError(error.reason);
+                }
+            });
+        }
+        //return false;
+    }
+
+});
--- server/users.js
+++ server/users.js
@@ -1,3 +1,27 @@
+ Accounts.loginServiceConfiguration.remove({
+        service: "facebook"
+    });
+    Accounts.loginServiceConfiguration.insert({
+        service: "facebook",
+        appId: "529657053789199",
+        secret: "7c718b0584900ed38e8ab256a4959de7"
+    });
+
+ Accounts.loginServiceConfiguration.remove({
+        service: "google"
+    });
+    Accounts.loginServiceConfiguration.insert({
+        service: "google",
+        clientId: "1073701798617.apps.googleusercontent.com",
+        secret: "OYcYBXSMAQT3umucSI68Z4S6"
+    });
+
+
+
+
+
+
+
 Accounts.onCreateUser(function(options, user) {
     user.tags = ["computers", "startups", "cooking"];
     user.postsRead = [];
