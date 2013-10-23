fbf52278871bef4b54bb5072e82b9bfa7ec2cd29
Ubuntu <ubuntu@ip-172-31-43-4.us-west-2.compute.internal>
2013-10-23 03:13:51 +0000

    existing users can use external login

--- server/users.js
+++ server/users.js
@@ -23,6 +23,30 @@
 
 
 Accounts.onCreateUser(function(options, user) {
+
+    if (user.services) {
+	var service = _.keys(user.services)[0];
+	var email = user.services[service].email;
+	
+	// see if any existing user has this email address, otherwise create new
+	var existingUser = Meteor.users.findOne({'emails.address': email});
+	if (existingUser) {
+	    // precaution, these will exist from accounts-password if used
+	    if (!existingUser.services)
+		existingUser.services = { resume: { loginTokens: [] }};
+	    
+	    // copy accross new service info
+	    existingUser.services[service] = user.services[service];
+	    existingUser.services.resume.loginTokens.push(
+		user.services.resume.loginTokens[0]
+	    );
+	    
+	    // even worse hackery
+	    Meteor.users.remove({_id: existingUser._id}); // remove existing record
+	    return existingUser;      // record is re-inserted
+	}
+    }
+    
     user.tags = ["computers", "startups", "cooking"];
     user.postsRead = [];
     user.taskTime = 25*60;
